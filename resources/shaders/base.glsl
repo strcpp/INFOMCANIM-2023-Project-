dfce7150b6cf7aab02bf40c53f86561f12617226
abed <abed.farhat96@gmail.com>
2023-06-01 17:11:27 +0200

    fragPos

--- resources/shaders/base.glsl
+++ resources/shaders/base.glsl
@@ -12,6 +12,7 @@ in ivec4 in_jointsIdx;
 
 out vec2 tex_coords;
 out vec3 normal;
+out vec3 fragPos;
 
 uniform mat4 model;
 uniform mat4 projection;
@@ -42,6 +43,7 @@ void main() {
     }
 
     normal = mat3(transpose(inverse(model))) * normalize(in_normal);
+    fragPos = vec3(model * totalPosition); 
 
     gl_Position = projection * view * model * totalPosition;
     tex_coords = in_texcoord_0;
