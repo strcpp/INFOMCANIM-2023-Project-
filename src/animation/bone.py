51e1ec3b0cf33b1a3752fba5b29d399f12ede40f
CharalamposGeorgiadis <georgfx@gmail.com>
2023-06-09 11:47:52 +0200

    Testing weird stuff

--- src/animation/bone.py
+++ src/animation/bone.py
@@ -105,40 +105,98 @@ class Bone:
 
                 tangent_translation = np.array([0, 0, 0])
 
+                translation_middle_1 = translation_p0.value * 0.67 + translation_p1.value * 0.33
+                translation_middle_2 = translation_p0.value * 0.34 + translation_p1.value * 0.66
+
+                tangent_translation_1 = ((translation_middle_1 - translation_p0.value) +
+                                         (translation_middle_2 - translation_middle_1)) / 2
+
+                tangent_translation_2 = ((translation_middle_2 - translation_middle_1) +
+                                         (translation_p1.value - translation_middle_2)) / 2
+
                 timestamp_norm = (timestamp - translation_p0.timestamp) / (translation_p1.timestamp -
                                                                            translation_p0.timestamp)
                 timestamp_norm = clip(timestamp_norm, 0.0, 1.0)
 
-                hermite_array = np.array([translation_p0.value,
-                                          translation_p1.value,
-                                          tangent_translation,
-                                          tangent_translation])
+                hermite_array = np.array([translation_p0.value - translation_p0.value,
+                                          translation_p1.value - translation_p0.value,
+                                          tangent_translation_1,
+                                          tangent_translation_2])
 
                 inter_translation = np.array([timestamp_norm ** 3,
                                               timestamp_norm ** 2,
-                                              timestamp_norm, 1.0]) @ coeff_array @ hermite_array
+                                              timestamp_norm, 1.0]) @ coeff_array @ hermite_array + translation_p0.value
 
                 tangent_rotation = np.array([0, 0, 0, 0])
 
-                hermite_array = np.array([rotation_p0.value,
-                                          rotation_p1.value,
-                                          tangent_rotation,
-                                          tangent_rotation])
+                rotation_middle_1 = rotation_p0.value * 0.67 + rotation_p1.value * 0.33
+                rotation_middle_2 = rotation_p0.value * 0.34 + rotation_p1.value * 0.66
+
+                from scipy.spatial.transform import Rotation
+
+                r0 = rotation_p0.value / np.linalg.norm(rotation_p0.value)
+                r1 = rotation_middle_1 / np.linalg.norm(rotation_middle_1)
+                r2 = rotation_middle_2 / np.linalg.norm(rotation_middle_2)
+                r3 = rotation_p1.value / np.linalg.norm(rotation_p1.value)
+
+                r1_sub_r0 = Rotation.from_quat(r1).inv() * Rotation.from_quat(r0)
+                angle = 2 * np.arccos(np.abs(r1_sub_r0.as_quat()[0]))
+                axis = r1_sub_r0.as_quat()[1:] / np.linalg.norm(r1_sub_r0.as_quat()[1:])
+                r1_sub_r0 = angle * axis
+
+                r2_sub_r1 = Rotation.from_quat(r2).inv() * Rotation.from_quat(r1)
+                angle = 2 * np.arccos(np.abs(r2_sub_r1.as_quat()[0]))
+                axis = r2_sub_r1.as_quat()[1:] / np.linalg.norm(r2_sub_r1.as_quat()[1:])
+                r2_sub_r1 = angle * axis
+
+                r3_sub_r2 = Rotation.from_quat(r3).inv() * Rotation.from_quat(r2)
+                angle = 2 * np.arccos(np.abs(r3_sub_r2.as_quat()[0]))
+                axis = r3_sub_r2.as_quat()[1:] / np.linalg.norm(r3_sub_r2.as_quat()[1:])
+                r3_sub_r2 = angle * axis
+
+                tangent_rotation_1 = (r1_sub_r0 + r2_sub_r1) / 2
+                tangent_rotation_2 = (r2_sub_r1 + r3_sub_r2) / 2
+
+                r0_sub_r0 = Rotation.from_quat(r0).inv() * Rotation.from_quat(r0)
+                angle = 2 * np.arccos(np.abs(r0_sub_r0.as_quat()[0]))
+                axis = r0_sub_r0.as_quat()[1:] / np.linalg.norm(r0_sub_r0.as_quat()[1:])
+                r0_sub_r0 = angle * axis
+
+                r3_sub_r0 = Rotation.from_quat(r3).inv() * Rotation.from_quat(r0)
+                angle = 2 * np.arccos(np.abs(r3_sub_r0.as_quat()[0]))
+                axis = r3_sub_r0.as_quat()[1:] / np.linalg.norm(r3_sub_r0.as_quat()[1:])
+                r3_sub_r0 = angle * axis
+
+                hermite_array = np.array([r0_sub_r0,
+                                          r3_sub_r0,
+                                          tangent_rotation_1,
+                                          tangent_rotation_2])
 
                 inter_rotation = np.array([timestamp_norm ** 3,
                                            timestamp_norm ** 2,
-                                           timestamp_norm, 1.0]) @ coeff_array @ hermite_array
+                                           timestamp_norm, 1.0]) @ coeff_array @ hermite_array + r0_sub_r0
 
-                tangent_scale =  np.array([0, 0, 0])
+                inter_rotation = Rotation.as_quat(Rotation.from_rotvec(inter_rotation))
 
-                hermite_array = np.array([scale_p0.value,
-                                          scale_p1.value,
-                                          tangent_scale,
-                                          tangent_scale])
+                tangent_scale = np.array([0, 0, 0])
+
+                scale_middle_1 = scale_p0.value * 0.67 + scale_p1.value * 0.33
+                scale_middle_2 = scale_p0.value * 0.34 + scale_p1.value * 0.66
+
+                tangent_scale_1 = ((scale_middle_1 - scale_p0.value) +
+                                   (scale_middle_2 - scale_middle_1)) / 2
+
+                tangent_scale_2 = ((scale_middle_2 - scale_middle_1) +
+                                   (scale_p1.value - scale_middle_2)) / 2
+
+                hermite_array = np.array([scale_p0.value - scale_p0.value,
+                                          scale_p1.value - scale_p0.value,
+                                          tangent_scale_1,
+                                          tangent_scale_2])
 
                 inter_scale = np.array([timestamp_norm ** 3,
                                         timestamp_norm ** 2,
-                                        timestamp_norm, 1.0]) @ coeff_array @ hermite_array
+                                        timestamp_norm, 1.0]) @ coeff_array @ hermite_array + scale_p0.value
 
                 from_translation(inter_translation, translation)
                 from_quaternion(inter_rotation, rotation)
